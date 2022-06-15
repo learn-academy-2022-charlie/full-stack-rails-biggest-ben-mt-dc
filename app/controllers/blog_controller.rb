bf3d7d2959d3a7a7d6b4cfa89e4caf693ee9d226
michael-a-tobias <michael.a.tobias@outlook.com>
2022-06-15 16:30:55 -0700

    RESTful route 'new' complete. Beginning 'create'.

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -7,4 +7,14 @@ class BlogController < ApplicationController
     def show
         @blog = Blog.find(params[:id])
     end
+
+    def new
+       @blog = Blog.new
+    end
+        # Code below to be implemented during "create"
+        # @blog = Blog.new(
+        # title: params[:title],
+        # content: params[:content]
+        # )
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'blog#index', as: 'home'
-
+  get 'blog/new' => 'blog#new', as: 'new_blog'
   get 'blog/:id' => 'blog#show', as: 'blog'
 end
