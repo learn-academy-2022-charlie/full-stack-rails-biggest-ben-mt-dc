be93dafbcb1063f0e359ac1ed288f3e73fecbfa7
Denise-Carlson <carlsonden@gmail.com>
2022-06-16 09:49:20 -0700

    completed create form and links work

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'blog#index', as: 'home'
   get 'blog/new' => 'blog#new', as: 'new_blog'
   get 'blog/:id' => 'blog#show', as: 'blog'
+  post 'blog' => 'blog#create'
 end
